09b6af1dccc5b5345d6d4c22cf43c46aa989a639
luckybulldozer <dan@luckybulldozer.com>
2014-09-13 11:40:20 +1200

    Init Commit
    starting server_sfm dir

---
+++ server-sfm.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# engine for executing from ec2-sfm.lib
+
+#known functions and execution order to be added.
+
+. ~/sfm/server-sfm.lib
+
+### WORKING FUNCTIONS
+
+#INIT ROUTINES
+
+#1
+initVars #MUST BE ON!!!
+#2
+initRm
+#echo "RM turned OFF!"
+#3
+initDirs
+
+# SERVER INIT ROUTINES
+
+#10
+##chooseNumberOfServers
+#20
+getImgList
+#30 - Assigns the IP addresses...
+##assignIPRange
+#39
+serverIDs
+#echo "end here?"
+#read ab
+#read av
+
+#40
+realServerCreation
+#fakeServerCreation
+#echo "end here?"
+#read ab
+#read av
+
+initRemoteServers
+
+#50
+copyImagesToRealServers
+#copyImagesToFakeServers
+#60
+siftListsPerServer
+#70
+copyListsToRealServers
+#copyListsToFakeServers
+
+# START SIFT ON SERVERS
+
+#80
+startRealSifts
+#startFakeSifts
+#90
+waitForSiftsToFinish
+
+echo END HERE?
+read noout
+
+#100
+getInverseSifts
+
+echo END AT getInverseSifts
+read nnout
+#110
+waitForSiftsToCopy
+
+echo END at waitForSiftsToCopy
+read nouut
+
+# START MATCH ON SERVERS
+
+#120
+getMatchListTotal
+#130
+makeMatchLists #depends on getTotalImgCount
+#140
+startMatchesOnServers
+#150
+waitForMatchesToExport
+#160
+combineMatch
+#170
+copyMatchestoRealServers
+#copyMatchesToFakeServers
+#180
+
+#CONSIDER MATCHES
+
+#REDISTUBITE VSFMS
+
+#COMBINE VSFMs
+
+#PMVS STAGE
+
+
+
+#vsfmMatchImages
+
+
+### eg a sift looper;
+#waitForMatchesToFinish
+#exportMatchesFromServer
+
+#waitForMatchesToFinish
+#exportMatchesFromServer
---
+++ sh_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+screen -S sfmSreen -d -m sh -c "while :; do ~/sw/sfm/sh_server.sh; done;"
+
+
