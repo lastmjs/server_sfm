739bb53ae41fd87e0bdd6f9a52e715e2bfdba578
luckybulldozer <dan@luckybulldozer.com>
2014-09-16 16:51:28 +1200

    got the screen command working ok...

--- sh_server.sh
+++ sh_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-screen -S sfmSreen -d -m sh -c "while :; do ~/sfm/server_sfm/launch_sh_server.sh; done;"
-
+open -a /Applications/Utilities/Terminal.app/ ~/sfm/server_sfm/termScreenGL.sh
+sleep 5; 
+#	screen -S spawner_GL -X 'command'` ~/sfm/server_sfm/launch_sh_server.sh'\015'`
+#screen -r spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh' 
+#screen -dmS spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh; exec bash'
+screen -r spawner_GL
 
