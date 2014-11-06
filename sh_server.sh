8817922e241e7466736228b34a8697934f53a8ed
Jordan Last <jordan.michael.last@gmail.com>
2014-11-06 13:24:16 -0700

    added linux check for slaves

--- sh_server.sh
+++ sh_server.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-open -a /Applications/Utilities/Terminal.app/ ~/sfm/server_sfm/termScreenGL.sh
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+    x-terminal-emulator --noclose
+else
+    open -a /Applications/Utilities/Terminal.app/
+fi
+
+~/sfm/server_sfm/termScreenGL.sh
 sleep 5; 
-#	screen -S spawner_GL -X 'command'` ~/sfm/server_sfm/launch_sh_server.sh'\015'`
+
+#screen -S spawner_GL -X 'command'` ~/sfm/server_sfm/launch_sh_server.sh'\015'`
 #screen -r spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh' 
 #screen -dmS spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh; exec bash'
-screen -r spawner_GL
 
+screen -r spawner_GL
