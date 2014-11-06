d37af19ff9c581dbe263d57a985c949c340cc80f
Jordan Last <jordan.michael.last@gmail.com>
2014-11-06 14:08:42 -0700

    renaming

--- termScreenGL.sh
+++ termScreenGL.sh
@@ -2,4 +2,4 @@
 #os x - terminal launcher for screen connect.
 #screen -dmS spawner_GL
 #screen -r spawner_
-screen -dmS spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh; exec bash'
+screen -dmS spawner_GL bash -c '~/sfm/server_sfm/launch_slave_server.sh; exec bash'
