bdbe7b49a08f3ebac797bf13f54d662f92ff1e9d
Jordan Last <jordan.michael.last@gmail.com>
2014-11-18 15:30:39 -0700

    testing

--- slave_server.sh
+++ slave_server.sh
@@ -7,11 +7,12 @@
 #    open -a /Applications/Utilities/Terminal.app/
 #fi
 
-~/sfm/server_sfm/termScreenGL.sh
-sleep 5; 
+#~/sfm/server_sfm/termScreenGL.sh
+#sleep 5; 
 
 #screen -S spawner_GL -X 'command'` ~/sfm/server_sfm/launch_sh_server.sh'\015'`
 #screen -r spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh' 
 #screen -dmS spawner_GL bash -c '~/sfm/server_sfm/launch_sh_server.sh; exec bash'
 
 screen -r spawner_GL
+screen -dmS spawner_GL ~/sfm/server_sfm/launch_slave_server.sh
